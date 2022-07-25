eec1627957606f8c47a41f52542a0e3e8082f817
naqib2000 <105971109+naqib2000@users.noreply.github.com>
2022-07-25 21:37:05 +0100

    setup react router and error page

--- OccuTrack/client/src/App.js
+++ OccuTrack/client/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Landing from "./pages/Landing";
+import { Dashboard, Register, Landing, Error } from "./pages"
 
 const App = () => {
   return (
-    <>
-        <Landing />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- OccuTrack/client/src/pages/Landing.js
+++ OccuTrack/client/src/pages/Landing.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import { Logo } from "../components";
 import main from "../assets/images/main.svg";
-import Wrapper from "../assets/wrappers/LandingPage"
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -16,7 +17,7 @@ const Landing = () => {
         <div className="info">
             <h1>Job <span>tracking</span> application</h1>
             <p>Without a dedicated job tracking system, it can be difficult to keep track of all your job applications at one time. A job tracking system gives you everything you need to keep track of you job applications. Spend less time tracking applications and more time doing them.</p>
-            <button className="btn btn-hero">Login/Register</button>
+            <Link to="/register" className="btn btn-hero">Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
        </div>
