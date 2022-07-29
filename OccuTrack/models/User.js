8e7f9caf461b60cc3de5f9aea94cc982ad135691
naqib2000 <105971109+naqib2000@users.noreply.github.com>
2022-07-29 16:29:05 +0100

    Create JWT

--- OccuTrack/models/User.js
+++ OccuTrack/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"; 
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -22,7 +23,8 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "please provide password"],
-        minlength: 6
+        minlength: 6,
+        select: false
     },
     lastName: {
         type: String,
@@ -41,6 +43,10 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre("save", async function(){
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-});
+}); 
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({userId:this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFTIME})
+}
 
 export default mongoose.model("User", UserSchema);
