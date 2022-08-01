48144e3ea53736debe832a7f236433724fa5225b
Muhammad <muhammadnaqib2000@gmail.com>
2022-08-01 23:27:44 +0100

    Create navbar

--- OccuTrack/client/src/context/appContext.js
+++ OccuTrack/client/src/context/appContext.js
@@ -7,7 +7,9 @@ import {
     DISPLAY_ALERT, 
     SETUP_USER_BEGIN,
     SETUP_USER_SUCCESS,
-    SETUP_USER_ERROR  
+    SETUP_USER_ERROR ,
+    TOGGLE_SIDEBAR,
+    LOGOUT_USER
 } from "./action";
 
 const token = localStorage.getItem("token");
@@ -22,7 +24,8 @@ const initialState = {
     user: user? JSON.parse(user) : null,
     token: token,
     userLocation: userLocation || "",
-    jobLocation: userLocation || ""
+    jobLocation: userLocation || "",
+    showSidebar: false
 };
 
 const AppContext = React.createContext();
@@ -73,8 +76,17 @@ const setupUser = async ({currentUser, endPoint, alertText}) => {
 clearAlert()
 }
 
+const toggleSidebar = () => {
+    dispatch({ type: TOGGLE_SIDEBAR });
+}
+
+const logoutUser = () => {
+    dispatch({ type: LOGOUT_USER });
+    removeUserFromLocalStorage()
+}
+
     return ( 
-        <AppContext.Provider value={{...state, displayAlert, setupUser}}>
+        <AppContext.Provider value={{...state, displayAlert, setupUser, toggleSidebar, logoutUser}}>
             {children}
         </AppContext.Provider>
     );
