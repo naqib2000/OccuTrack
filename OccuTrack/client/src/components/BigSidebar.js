fa968e5d7841fddedb06f2a504d1b89bcf5ed9d2
Muhammad <muhammadnaqib2000@gmail.com>
2022-08-01 16:50:02 +0100

    Create shared layout for nested pages

---
+++ OccuTrack/client/src/components/BigSidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Wrapper from "../assets/wrappers/BigSidebar";
+
+const BigSidebar = () => {
+  return (
+    <Wrapper>
+       <h4>big sidebar</h4> 
+    </Wrapper>
+  );
+};
+
+export default BigSidebar;
---
+++ OccuTrack/client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaHome } from "react-icons/fa";
+
+import Wrapper from "../assets/wrappers/Navbar";
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+       <h4>navbar</h4> 
+       <FaHome />
+    </Wrapper>
+  );
+};
+
+export default Navbar;
