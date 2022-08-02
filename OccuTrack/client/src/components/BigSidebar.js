6e72a1a9c5fa7ed25f365646de345000441767e7
Muhammad <muhammadnaqib2000@gmail.com>
2022-08-02 18:22:43 +0100

    Create big sidebar

--- OccuTrack/client/src/components/BigSidebar.js
+++ OccuTrack/client/src/components/BigSidebar.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
 import Wrapper from "../assets/wrappers/BigSidebar";
 
 const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-       <h4>big sidebar</h4> 
+      <div
+        className={
+          showSidebar ? "sidebar-container " : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- OccuTrack/client/src/components/SmallSidebar.js
+++ OccuTrack/client/src/components/SmallSidebar.js
@@ -1,48 +1,31 @@
 import React from "react";
-import { FaTimes } from "react-icons/fa"
-import { NavLink } from "react-router-dom"
+import { FaTimes } from "react-icons/fa";
 
-import { useAppContext } from "../context/appContext"
-import links from "../utils/links"
-import Logo from "./Logo"
-
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
-  const {showSidebar, toggleSidebar} = useAppContext();
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-       <div className={showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"}>
-          <div className="content">
-            <button 
-            type="button"
-            className="close-btn"
-            onClick={toggleSidebar}  
-            >
-              <FaTimes />
-            </button>
-            <header>
-              <Logo />
-            </header>
-            <div className="nav-links">
-              {links.map((link) => {
-                const {text, path, id, icon} = link
-                return (
-                  <NavLink 
-                    to={path} 
-                    key={id} 
-                    onClick={toggleSidebar}
-                    className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
-                  >
-                    <span className="icon">{icon}</span>
-                    {text}
-                  </NavLink>
-                );
-              })}
-            </div>
-          </div>
-       </div>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
