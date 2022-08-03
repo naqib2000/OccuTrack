a361e35d5e9656744be32989686ac275f156adf6
Muhammad <muhammadnaqib2000@gmail.com>
2022-08-03 15:12:54 +0100

    Create profile page

--- OccuTrack/client/src/context/appContext.js
+++ OccuTrack/client/src/context/appContext.js
@@ -2,14 +2,14 @@ import React, { useReducer, useContext } from "react";
 
 import reducer from "./reducer";
 import axios from "axios";
-import { 
-    CLEAR_ALERT, 
-    DISPLAY_ALERT, 
-    SETUP_USER_BEGIN,
-    SETUP_USER_SUCCESS,
-    SETUP_USER_ERROR ,
-    TOGGLE_SIDEBAR,
-    LOGOUT_USER
+import {
+  CLEAR_ALERT,
+  DISPLAY_ALERT,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
 } from "./action";
 
 const token = localStorage.getItem("token");
@@ -17,84 +17,98 @@ const user = localStorage.getItem("user");
 const userLocation = localStorage.getItem("location");
 
 const initialState = {
-    isLoading: false,
-    showAlert: false,
-    alertText: "",
-    alertType: "",
-    user: user? JSON.parse(user) : null,
-    token: token,
-    userLocation: userLocation || "",
-    jobLocation: userLocation || "",
-    showSidebar: false
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || "",
+  showSidebar: false,
 };
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const displayAlert = () => {
-    dispatch({type: DISPLAY_ALERT});
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
     clearAlert();
-    
-}
+  };
 
-const clearAlert = () => {
+  const clearAlert = () => {
     setTimeout(() => {
-        dispatch({type: CLEAR_ALERT});
-    }, 3000)
-}
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
+  };
 
-const addUserToLocalStorage = ({user,token,location}) => {
+  const addUserToLocalStorage = ({ user, token, location }) => {
     localStorage.setItem("user", JSON.stringify(user));
     localStorage.setItem("token", token);
     localStorage.setItem("location", location);
-};
+  };
 
-const removeUserFromLocalStorage = () => {
+  const removeUserFromLocalStorage = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     localStorage.removeItem("location");
-};
+  };
 
-const setupUser = async ({currentUser, endPoint, alertText}) => {
-  dispatch({ type: SETUP_USER_BEGIN })
-  try {
-    const {data} = await axios.post(`/api/v1/auth/${endPoint}`, currentUser);
-    const {user,token,location} = data;
-    dispatch({ 
-      type: SETUP_USER_SUCCESS, 
-      payload: {user, token, location, alertText}
-    })
-    addUserToLocalStorage({user, token, location})
-  } catch (error) {
-  dispatch({
-    type: SETUP_USER_ERROR,
-    payload: { msg: error.response.data.msg },
-  })
-}
-clearAlert()
-}
+  const setupUser = async ({ currentUser, endPoint, alertText }) => {
+    dispatch({ type: SETUP_USER_BEGIN });
+    try {
+      const { data } = await axios.post(
+        `/api/v1/auth/${endPoint}`,
+        currentUser
+      );
+      const { user, token, location } = data;
+      dispatch({
+        type: SETUP_USER_SUCCESS,
+        payload: { user, token, location, alertText },
+      });
+      addUserToLocalStorage({ user, token, location });
+    } catch (error) {
+      dispatch({
+        type: SETUP_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
 
-const toggleSidebar = () => {
+  const toggleSidebar = () => {
     dispatch({ type: TOGGLE_SIDEBAR });
-}
+  };
 
-const logoutUser = () => {
+  const logoutUser = () => {
     dispatch({ type: LOGOUT_USER });
-    removeUserFromLocalStorage()
-}
+    removeUserFromLocalStorage();
+  };
 
-    return ( 
-        <AppContext.Provider value={{...state, displayAlert, setupUser, toggleSidebar, logoutUser}}>
-            {children}
-        </AppContext.Provider>
-    );
+  const updateUser = async (currentUser) => {
+    console.log(currentUser);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        displayAlert,
+        setupUser,
+        toggleSidebar,
+        logoutUser,
+        updateUser,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useAppContext = () => {
-    return useContext(AppContext);
+  return useContext(AppContext);
 };
 
-export { AppProvider, initialState, useAppContext }
-
+export { AppProvider, initialState, useAppContext };
