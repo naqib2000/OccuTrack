a8e6d1cce95aa0255cb6e3c0c17973bf045b4355
Muhammad <muhammadnaqib2000@gmail.com>
2022-08-15 22:59:44 +0100

    Create edit and delete functionality front end side

--- OccuTrack/client/src/context/reducer.js
+++ OccuTrack/client/src/context/reducer.js
@@ -17,6 +17,10 @@ import {
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
+  DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_SUCCESS,
+  EDIT_JOB_ERROR,
 } from "./action";
 
 import { initialState } from "./appContext";
@@ -165,12 +169,39 @@ const reducer = (state, action) => {
     return {
       ...state,
       isEditing: true,
-      editJob: _id,
+      editJobId: _id,
       position,
       company,
       jobLocation,
       jobType,
       status,
+    };
+  }
+  if (action.type === DELETE_JOB_BEGIN) {
+    return { ...state, isLoading: true };
+  }
+  if (action.type === EDIT_JOB_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === EDIT_JOB_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "success",
+      alertText: "Job Updated!",
+    };
+  }
+  if (action.type === EDIT_JOB_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
     };
   }
   throw new Error(`no such action : ${action.type}`);
