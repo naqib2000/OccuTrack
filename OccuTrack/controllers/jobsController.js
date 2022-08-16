2ddc2f2d8cfada6f49c0708cb5037284a7aaea75
Muhammad <muhammadnaqib2000@gmail.com>
2022-08-16 16:31:18 +0100

    Aggregate jobs for year and month server side

--- OccuTrack/controllers/jobsController.js
+++ OccuTrack/controllers/jobsController.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import mongoose from "mongoose";
+import moment from "moment";
 
 import Job from "../models/Job.js";
 import {
@@ -84,7 +85,32 @@ const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  let monthlyApplications = [];
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
